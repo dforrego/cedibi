5be0db77062ae90d01236002aeee4ad1a0cafbf3
Andres Martinez <anmartinez@novopayment.com>
2019-12-15 20:48:29 -0500

    Crear modulos con modelos

---
+++ dashboard/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from login.models import User
+
+
+def user_directory_path(instance, filename):
+    return 'company_{0}/{1}'.format(instance.nit, filename)
+
+
+class Menu(models.Model):
+    icon = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+    path = models.CharField(max_length=50)
+    parent_id = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+    enable = models.BooleanField(default=True)
+    
+    
+class Rol(models.Model):
+    name = models.CharField(max_length=20)
+    code = models.IntegerField()
+    permission = models.ManyToManyField(
+        Menu,
+        through='Permission',
+        through_fields=('rol', 'menu'),
+    )
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=50)
+    nit = models.CharField(max_length=9)
+    logo = models.ImageField(upload_to=user_directory_path)
+    description = models.CharField(max_length=9)
+    status = models.BooleanField(default=True)
+    contact_name = models.CharField(max_length=30)
+    contact_email = models.CharField(max_length=40)
+    contact_phone = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, editable=False)
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rol = models.ForeignKey(Rol, on_delete=models.PROTECT)
+    phone = models.CharField(max_length=12)
+    company = models.ForeignKey(Company, on_delete=models.PROTECT)
+    group = models.CharField(max_length=30)
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, editable=False)
+
+
+class Permission(models.Model):
+    rol = models.ForeignKey(Rol, on_delete=models.CASCADE, related_name='+')
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    enable = models.BooleanField(default=True)
---
+++ login/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, editable=False)
+    
+    
+class ControlAccess(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    ip_address = models.CharField(max_length=50)
+    browser = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, editable=False)
