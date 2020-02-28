8c786b714e27e753c0c2a2ec509939a63c1c1c0e
Andres Martinez <anmartinez@novopayment.com>
2020-02-27 22:02:56 -0500

    Agrega auth a servicios de BI

--- bi/views.py
+++ bi/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from oauth2_provider.contrib.rest_framework import IsAuthenticatedOrTokenHasScope
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, ListAPIView
 from bi.models import Board, Graph, Comment
 from bi.serializers import BoardSerializer, GraphSerializer, CommentSerializer
@@ -6,6 +7,8 @@ from bi.serializers import BoardSerializer, GraphSerializer, CommentSerializer
 
 class BoardListCreate(ListCreateAPIView):
     serializer_class = BoardSerializer
+    permission_classes = (IsAuthenticatedOrTokenHasScope,)
+    required_scopes = ['read']
     
     def get_queryset(self):
         values = Board.objects.filter(rol=self.kwargs["pk"])
@@ -13,8 +16,13 @@ class BoardListCreate(ListCreateAPIView):
 
 
 class BoardObject(RetrieveAPIView):
-    queryset = Board.objects.filter(pk=1)
     serializer_class = BoardSerializer
+    permission_classes = (IsAuthenticatedOrTokenHasScope,)
+    required_scopes = ['read']
+    
+    def get_queryset(self):
+        values = Board.objects.filter(pk=self.kwargs["pk"])
+        return get_object_or_404(values)
   
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
@@ -22,6 +30,8 @@ class BoardObject(RetrieveAPIView):
 
 class GraphList(ListAPIView):
     serializer_class = GraphSerializer
+    permission_classes = (IsAuthenticatedOrTokenHasScope,)
+    required_scopes = ['read']
     
     def get_queryset(self):
         values = Graph.objects.filter(board=self.kwargs["pk"])
@@ -30,6 +40,8 @@ class GraphList(ListAPIView):
 
 class CommentListCreate(ListCreateAPIView):
     serializer_class = CommentSerializer
+    permission_classes = (IsAuthenticatedOrTokenHasScope,)
+    required_scopes = ['read']
   
     def get_queryset(self):
         values = Comment.objects.filter(board=self.kwargs["pk"])
